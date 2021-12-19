0ef38aee02e7881c6f82af1eb9cef54fb4cd461e
Firemark <marpiechula@gmail.com>
2021-12-19 19:14:21 +0100

    add randomized colors

--- procedural_pixel_art/colors.py
+++ procedural_pixel_art/colors.py
@@ -1,39 +1,61 @@
-from PIL import ImageColor
+from PIL import Image, ImageColor
 from typing import List, Tuple
-from random import randint
+from random import random, uniform
+from array import array
 
-
-def get_hex_pallets():
-	return [
-		("DDA459", "B0975A", "D6975B"),
-		("4EAEB2", "ADEFAF", "F0F5DF"),
-		("7C8064", "B9C095", "EED190"),
-		("11396C", "1F6378", "6F6B4D"),
-		("585C59", "617974", "C2DFC4"),
-		("803C50", "EC8357", "F0DC4E"),
-		("C1D09E", "B4E49D", "8EB893"),
-		("A7A986", "88304B", "42382C"),
-		("38453F", "397B87", "A1CFB7"),
-		("171A2A", "2F3D52", "3E5C84")
-	]
 
 def get_random_colors() -> List[Tuple[int, int, int]]:
-	"""Get rgb colors from api. Apie return 5 colors"""
-	print("Getting colors")
-	hex_pallets = get_hex_pallets()
-	number_of_pallets = len(hex_pallets)
-	random_index = randint(0, number_of_pallets - 1)
-	random_pallet = hex_pallets[random_index]
-	return parse_hex_to_rgb(random_pallet)
+    base_color = get_random_color()
+    r = random()
+    if r < 0.33:
+        complementary_color = get_complementary_color(base_color)
+        colors = base_color, mutate_color(base_color), mutate_color(complementary_color)
+    elif r < 0.66:
+        second_color, third_color = get_triadic_colors(base_color)
+        colors = base_color, mutate_color(second_color), mutate_color(third_color)
+    else:
+        colors = base_color, mutate_color(base_color), mutate_color(base_color)
+
+    return [hsv_to_rgb(color) for color in colors]
 
 
-def parse_hex_to_rgb(hex_colors):
-	colors = []
-	for hex_color in hex_colors:
-		rgb_color = ImageColor.getcolor(f"#{hex_color}", "RGB")
-		colors.append(rgb_color)
-	return colors
+def get_random_color():
+    h = random()
+    s = uniform(0.5, 0.8)
+    v = uniform(0.5, 0.8)
+    return (h, s, v)
+
+
+def get_complementary_color(base_color):
+    h, s, v = base_color
+    h = (h + 0.5) % 1.0
+    return (h, s, v)
+
+
+def get_triadic_colors(base_color):
+    h, s, v = base_color
+    h1 = (h + 0.33) % 1.0
+    h2 = (h + 0.66) % 1.0
+    return ((h1, s, v), (h2, s, v))
+
+
+def mutate_color(color):
+    h, s, v = color
+    h = (h + uniform(-0.1, 0.1)) % 1.0
+    s += uniform(-0.5, +0.2)
+    v += uniform(-0.5, +0.2)
+    return (h, s, v)
+
+
+def hsv_to_rgb(color) -> Tuple[int, int, int]:
+    h, s, v = color
+    css_format = f"hsv({int(h * 360)},{int(s * 100)}%,{int(v * 100)}%)"
+    return ImageColor.getrgb(css_format)
 
 
 if __name__ == "__main__":
-	print(get_random_colors())
+    pallete = get_random_colors()
+    arr = array("B", pallete[0] + pallete[1] + pallete[2])
+    img = Image.frombytes("RGB", (3, 1), bytes(arr))
+    img = img.resize((256, 256), resample=Image.NEAREST)
+    img.show()
